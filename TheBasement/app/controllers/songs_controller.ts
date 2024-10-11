c9d02abb1b1fdecbff4c3f7681085d9d2e962c2b
Victor Miguez <v.miguez2004@gmail.com>
2024-10-11 20:03:08 -0300

    .

--- TheBasement/app/controllers/songs_controller.ts
+++ TheBasement/app/controllers/songs_controller.ts
@@ -2,7 +2,14 @@ import type { HttpContext } from '@adonisjs/core/http'
 import Song from '#models/song'
 
 export default class SongsController {
-    async create({  }: HttpContext) {
+    async index({ view, request }: HttpContext) {
+        const page = request.input('page', 1)
+        const limit = 10
+
+        
+    }
     
+    async create({  }: HttpContext) {
+        
     }
   }
