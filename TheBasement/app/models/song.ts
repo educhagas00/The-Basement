c42669114ed0f38fc7c84b8b7537f4ee734c572d
Victor Miguez <v.miguez2004@gmail.com>
2024-10-17 16:46:59 -0300

    refactor: remove price e release date de songs

--- TheBasement/app/models/song.ts
+++ TheBasement/app/models/song.ts
@@ -10,19 +10,10 @@ export default class Song extends BaseModel {
   declare name: string
 
   @column()
-  declare price: number
-
-  @column()
   declare duration: number
-
-  @column({ columnName: 'release_date' })
-  declare releaseDate: DateTime
 
   // @column({ columnName: 'genre_id' })
   // declare genreId: number
-
-  @column({ columnName: 'cover_path' })
-  declare coverPath: string
 
   @column({ columnName: 'album_id' })
   declare albumId: string
