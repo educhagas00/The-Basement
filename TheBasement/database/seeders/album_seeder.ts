80f7a1437e3904fee17bef6be2487a3c86686e7d
Victor Miguez <v.miguez2004@gmail.com>
2024-10-17 16:50:29 -0300

    refactor: song_seeder passa a buscar os album ids na tabela de álbuns para adicionar músicas no banco

--- TheBasement/database/seeders/album_seeder.ts
+++ TheBasement/database/seeders/album_seeder.ts
@@ -15,7 +15,7 @@ export default class extends BaseSeeder {
     const albumIds = ['2nLOHgzXzwFEpl62zAgCEC', '5zi7WsKlIiUXv09tbGLKsE', '48D1hRORqJq52qsnUYZX56' , '3nUNxSh2szhmN7iifAKv5i', '1bt6q2SruMsBtcerNVtpZB', '4LH4d3cOWNNsVw41Gqt2kv', '1VW1MFNstaJuygaoTPkdCk' ,
     '6YlDIxqEjvY63ffH6AwCjd', '3iPSVi54hsacKKl1xIR2eH', '2ANVost0y2y52ema1E9xAZ', '75ol9OP8bJaRqzGimpFHDm' , '4xwx0x7k6c5VuThz5qVqmV', '3AQgdwMNCiN7awXch5fAaG', '0JwHz5SSvpYWuuCNbtYZoV',
     '5Dgqy4bBg09Rdw7CQM545s', '03gwRG5IvkStFnjPmgjElw','0ETFjACtuP2ADo6LFhL6HN','6FCzvataOZh68j8OKzOt9a','19bQiwEKhXUBJWY6oV3KZk',
-    '0l3vDkpxvfbKyDTdOHSZhk','1UcS2nqUhxrZjrBZ3tHk2N','2UJcKiJxNryhL050F5Z1Fk','7otIoNh7kZgNoiNPauPaHU','6722nY178mLes49GPldnjj','2wnq5e000z2hT7qS2F8jZ5' ]
+    '0l3vDkpxvfbKyDTdOHSZhk','1UcS2nqUhxrZjrBZ3tHk2N','2UJcKiJxNryhL050F5Z1Fk','7otIoNh7kZgNoiNPauPaHU','6722nY178mLes49GPldnjj','2wnq5e000z2hT7qS2F8jZ5', '2tm3Ht61kqqRZtIYsBjxEj', '6uSnHSIBGKUiW1uKQLYZ7w', '3HZKOk1knxrUU3y5ZIOdbz', '5Y0p2XCgRRIjna91aQE8q7']
     
     for (const albumId of albumIds) {
       try {
@@ -32,7 +32,11 @@ export default class extends BaseSeeder {
         album.albumId = albumData.id
         album.name = albumData.name
         album.price = parseFloat((50 + Math.random() * 50).toFixed(2)) // numero aleatorio de 50 a 100 com 2 casas decimais
-        // album.duration = parseFloat((albumData.duration_ms / 60000).toFixed(2)) // Convertendo de milissegundos para segundos
+        let duration = 0
+        albumData.tracks.items.forEach((track: any) => {
+          duration += track.duration_ms
+        })
+        album.duration = duration / 60000 // duration em minutos
         album.releaseDate = albumData.release_date
 
         album.coverPath = albumData.images[0].url
