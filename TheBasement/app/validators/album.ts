da827cd3905de761e7484030f4887e9f8f048b55
educhagas00 <educhagas00@gmail.com>
2024-11-04 23:25:58 -0300

    feat: add album url validator. render not working

---
+++ TheBasement/app/validators/album.ts
@@ -0,0 +1,22 @@
+import vine from '@vinejs/vine'
+
+/**
+ * Validates the album creation action
+ */
+
+export const urlAlbumValidator = vine.compile( 
+    vine.object({
+        albumId: vine.string().trim().minLength(5),
+    })
+)
+
+
+export const createAlbumValidator = vine.compile(
+    vine.object({
+        name: vine.string().trim().minLength(3),
+        price: vine.number().min(0),
+        duration: vine.number().min(0),
+        releaseDate: vine.date(),
+        coverPath: vine.string().trim().minLength(5),
+    })
+)
