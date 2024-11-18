df12174484e8bd68808173f20efa1da016387a7e
educhagas00 <educhagas00@gmail.com>
2024-11-18 14:46:11 -0300

    fix: login viwer

--- TheBasement/app/controllers/auth_controller.ts
+++ TheBasement/app/controllers/auth_controller.ts
@@ -1,3 +1,6 @@
+import authConfig from '#config/auth'
+import User from '#models/user'
+import { createAuthValidator } from '#validators/auth'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AuthController {
@@ -5,12 +8,19 @@ export default class AuthController {
  
 
     async create({ view }: HttpContext) {
-        return view.render('pages/auth/register')
+        return view.render('pages/auth/login')
     }
 
-    async store({ request }: HttpContext) {
+    async store({ request, response, auth }: HttpContext) {
 
-        
+        const payload = await request.validateUsing(createAuthValidator)
+
+        const user = await User.verifyCredentials(payload.email, payload.password)
+
+        await auth.use('web').login(user) // auth eh uma classe que se extende pelo httpcontext
+
+        return response.redirect().toRoute('albums.index')
+
     }
 
     async destroy({ view }: HttpContext) {
--- TheBasement/database/seeders/album_seeder.ts
+++ TheBasement/database/seeders/album_seeder.ts
@@ -12,7 +12,7 @@ export default class extends BaseSeeder {
       } // o token de acesso (lá no .env) dura 1 hora! fazer o curl pelo terminal para pegar o token
     };
 
-    const albumIds = ['2nLOHgzXzwFEpl62zAgCEC', '5zi7WsKlIiUXv09tbGLKsE','4OjzAZXWalqQN5STXHPJdl', '48D1hRORqJq52qsnUYZX56' , '3nUNxSh2szhmN7iifAKv5i', '1bt6q2SruMsBtcerNVtpZB', '4LH4d3cOWNNsVw41Gqt2kv', '1VW1MFNstaJuygaoTPkdCk' ,
+    const albumIds = ['2nLOHgzXzwFEpl62zAgCEC', '5zi7WsKlIiUXv09tbGLKsE','0U28P0QVB1QRxpqp5IHOlH', '48D1hRORqJq52qsnUYZX56' , '3nUNxSh2szhmN7iifAKv5i', '1bt6q2SruMsBtcerNVtpZB', '4LH4d3cOWNNsVw41Gqt2kv', '1VW1MFNstaJuygaoTPkdCk' ,
     '6YlDIxqEjvY63ffH6AwCjd', '3iPSVi54hsacKKl1xIR2eH', '2ANVost0y2y52ema1E9xAZ', '75ol9OP8bJaRqzGimpFHDm' , '4xwx0x7k6c5VuThz5qVqmV', '3AQgdwMNCiN7awXch5fAaG', '0JwHz5SSvpYWuuCNbtYZoV',
     '5Dgqy4bBg09Rdw7CQM545s', '03gwRG5IvkStFnjPmgjElw','0ETFjACtuP2ADo6LFhL6HN','6FCzvataOZh68j8OKzOt9a','19bQiwEKhXUBJWY6oV3KZk','0l3vDkpxvfbKyDTdOHSZhk','1UcS2nqUhxrZjrBZ3tHk2N','2UJcKiJxNryhL050F5Z1Fk',
     '7otIoNh7kZgNoiNPauPaHU','6722nY178mLes49GPldnjj','2wnq5e000z2hT7qS2F8jZ5', '2tm3Ht61kqqRZtIYsBjxEj', '6uSnHSIBGKUiW1uKQLYZ7w', '3HZKOk1knxrUU3y5ZIOdbz', '5Y0p2XCgRRIjna91aQE8q7', '45ba6QAtNrdv6Ke4MFOKk9', '0ge5ZLyto8vFQrif125mtW' , '0I7bofr4iucY3LXUPKv9cz', '5Dbax7G8SWrP9xyzkOvy2F', '2jnV6ytZOmt71iEC5xHEYz', '1D3JNjKm7QRaHcIBzv0Xdn', '0lZRCf7prVEVVYjH5Im0TS', '1jWmEhn3ggaL6isoyLfwBn', '1zcm3UvHNHpseYOUfd0pna',
